62c1549107e30e207f7f2a867d867db75313f4d3
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-23 16:53:41 +0200

    Creazione array e prompt

--- myscript.js
+++ myscript.js
@@ -1,1 +1,14 @@
-document.getElementById('mioid').innerHTML = 'ciao';
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi
+// e stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+// Creo un array con i cognomi
+
+var lista = ['Rossi', 'Verdi', 'Bianchi' 'Bellini', 'Rossini', 'Puccini']
+
+// Chiedi all’utente il cognome
+
+var cognome = prompt('Scrivi il cognome');
+
+// inseriscilo in un array con altri cognomi
